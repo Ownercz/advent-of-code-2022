5fe3c671774ce6074bc46418b4f0f8202250ad47
Radim Lipovčan <radim.lipovcan@firma.seznam.cz>
2022-12-01 08:47:29 +0100

    Day 1 - part one

---
+++ day-1/day-1.py
@@ -0,0 +1,19 @@
+input_path = 'input'
+input_file_content = open(input_path,'r')
+input_content = input_file_content.read().splitlines()
+
+calories_sum = []
+single = 0
+final = len(input_content)-1
+
+for entry in input_content:
+    if entry != '':
+        single = single + int(entry)
+    else:
+        calories_sum.append(single)
+        single = 0
+    if(input_content.index(entry)==final):
+        calories_sum.append(single)
+
+calories_sum.sort()
+print(calories_sum)
