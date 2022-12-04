ac967b5cef635036d60cd209349cc85dcd3af115
Radim Lipovčan <radim@lipovcan.cz>
2022-12-04 09:40:12 +0100

    Day 4 - part 2 complete

---
+++ day-4/part-2.py
@@ -0,0 +1,35 @@
+
+def read_input():
+    input_path = '/home/ownercz/Seafile/Package/advent-of-code-2022/day-4/input'
+    input_file_content = open(input_path,'r')
+    return(input_file_content.read().splitlines())
+
+def line_to_set(entry_line):
+    ranges = entry_line.split(',')
+    elves = []
+    for range in ranges:
+        range_start = int(range.split('-')[0])
+        range_end = int(range.split('-')[1])
+        range_final = set()
+        while range_start <= range_end:
+            range_final.add(range_start)
+            range_start = range_start + 1
+        elves.append(range_final)
+    return elves
+
+def check_subset(elves):
+    if elves[0].issubset(elves[1]): return 1
+    elif elves[1].issubset(elves[0]): return 1
+    else: return 0
+
+def check_overlap(elves):
+    if len(elves[0].intersection(elves[1])) > 0:
+        return 1
+    else: return 0
+
+if __name__ == "__main__":
+    sum = 0
+    overlaps = []
+    for entry_line in read_input():
+        sum = sum + check_overlap(line_to_set(entry_line))
+    print(sum)
