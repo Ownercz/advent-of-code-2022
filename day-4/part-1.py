e7d923065dcc1a0d0ebeea83b4493776746288a3
Radim Lipovčan <radim@lipovcan.cz>
2022-12-04 09:31:07 +0100

    Day 4 - elves cleanup

--- day-4/part-1.py
+++ day-4/part-1.py
@@ -7,7 +7,6 @@ def read_input():
 def line_to_set(entry_line):
     ranges = entry_line.split(',')
     elves = []
-    print(entry_line)
     for range in ranges:
         range_start = int(range.split('-')[0])
         range_end = int(range.split('-')[1])
@@ -16,20 +15,15 @@ def line_to_set(entry_line):
             range_final.add(range_start)
             range_start = range_start + 1
         elves.append(range_final)
-    print(elves)
     return elves
 
 def check_subset(elves):
-    if elves[0].issubset(elves[1]): print("yes") ;return 1
-    elif elves[1].issubset(elves[0]): print("yes") ;return 1
-    else: print("no"); return 0
-
-def sum_count(entry,sum):
-    return sum+entry
+    if elves[0].issubset(elves[1]): return 1
+    elif elves[1].issubset(elves[0]): return 1
+    else: return 0
 
 if __name__ == "__main__":
     sum = 0
     for entry_line in read_input():
-        #sum = sum_count(check_subset(line_to_set(entry_line)),sum)
         sum = sum + check_subset(line_to_set(entry_line))
     print(sum)
